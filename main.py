c8d84c73eb2e7fc1491a81443f835718b31fd58e
cyx2015s <31377746+cyx2015s@users.noreply.github.com>
2023-03-05 20:57:53 +0800

    main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from fractions import Fraction as Frc
+
+
+def fac(n):
+    res = 1
+    for i in range(1, n + 1):
+        res *= n
+    return res
+
+
+def comb(n, m):
+    return fac(n) // fac(m) // fac(n - m)
+
+
+def hyper(n, N, M, k):
+    return Frc(comb(M, k) * comb(N - M, n - k), comb(N, n))
+
